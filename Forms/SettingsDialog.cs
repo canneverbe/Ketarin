71d469977816216dc7f62931cdaf5113b59ea2a6
floele <floele@users.noreply.github.com>
2008-11-08 13:27:17 +0000

    +Added settings

---
+++ Forms/SettingsDialog.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using CDBurnerXP;
+
+namespace Ketarin.Forms
+{
+    public partial class SettingsDialog : Form
+    {
+        public SettingsDialog()
+        {
+            InitializeComponent();
+
+            AcceptButton = bOK;
+            CancelButton = bCancel;
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            txtDefaultCommand.Text = Settings.GetValue("DefaultCommand", "") as string;
+            chkUpdateAtStartup.Checked = (bool)Settings.GetValue("UpdateAtStartup", false);
+
+        }
+
+        private void bOK_Click(object sender, EventArgs e)
+        {
+            Settings.SetValue("DefaultCommand", txtDefaultCommand.Text);
+            Settings.SetValue("UpdateAtStartup", chkUpdateAtStartup.Checked);
+        }
+    }
+}
