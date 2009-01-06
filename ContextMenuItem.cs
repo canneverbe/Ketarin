7cd663a45e50641269407d409177a823cb4ce339
floele <floele@758197f2-88c6-11dd-83f7-11f20a0db3b5>
2009-01-06 19:35:13 +0000

    +Insert variables per context menu

---
+++ ContextMenuItem.cs
@@ -0,0 +1,164 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+
+namespace Ketarin.Forms
+{
+    /// <summary>
+    /// Represents a custom item within a native context menu.
+    /// </summary>
+    public class ContextMenuItem
+    {
+        /// <summary>
+        /// Delegate for a function, which handles the click event for a menu item.
+        /// </summary>
+        /// <param name="menuItem">Item which has been clicked</param>
+        public delegate void ItemSelectHandler(ContextMenuItem menuItem);
+
+        private short m_Id = 0;
+        private int m_Position = -1;
+        private string m_Text = string.Empty;
+        private ItemSelectHandler m_Handler = null;
+        private List<ContextMenuItem> m_Items = new List<ContextMenuItem>();
+        private object m_Tag;
+
+        #region Properties
+
+        /// <summary>
+        /// User defined object which belongs to the menu item.
+        /// </summary>
+        public object Tag
+        {
+            get
+            {
+                return m_Tag;
+            }
+            set
+            {
+                m_Tag = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets the list of sub menu items.
+        /// </summary>
+        public List<ContextMenuItem> MenuItems
+        {
+            get
+            {
+                return m_Items;
+            }
+        }
+
+        /// <summary>
+        /// Gets the ID of the menu item. The value can be used
+        /// to identify a menu item.
+        /// </summary>
+        public short Id
+        {
+            get
+            {
+                return m_Id;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the position within the menu.
+        /// -1 is at the end, 0 at the start.
+        /// </summary>
+        public int Position
+        {
+            get
+            {
+                return m_Position;
+            }
+            set
+            {
+                m_Position = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the textual representation of the menu item.
+        /// An empty string represents a separator.
+        /// </summary>
+        public string Text
+        {
+            get
+            {
+                return m_Text;
+            }
+            set
+            {
+                m_Text = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the function, which is executed when the
+        /// item is clicked.
+        /// </summary>
+        public ItemSelectHandler EventHandler
+        {
+            get
+            {
+                return m_Handler;
+            }
+            set
+            {
+                m_Handler = value;
+            }
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Creates a new context menu item with the given text and given position.
+        /// </summary>
+        public ContextMenuItem(string text, int position) : this(text)
+        {
+            m_Position = position;
+        }
+
+        /// <summary>
+        /// Creates a new context menu item with the given text. Inserted at the end.
+        /// </summary>
+        public ContextMenuItem(string text)
+        {
+            m_Text = text;
+            m_Id = ContextMenuCustomiser.GetNextId();
+        }
+
+        /// <summary>
+        /// Inserts this menu item and all sub items into the given menu.
+        /// </summary>
+        /// <param name="hmenu">Handle of a Win32 context menu</param>
+        internal void InsertMenuItem(IntPtr hmenu)
+        {
+            if (string.IsNullOrEmpty(Text))
+            {
+                // Separator
+                User32.InsertMenu(hmenu, Position, (uint)(User32.MenuFlags.MF_BYPOSITION | User32.MenuFlags.MF_SEPARATOR), new IntPtr(User32.WM_USER + Id), null);
+            }
+            else
+            {
+                // Do we have submenus?
+                if (m_Items.Count > 0)
+                {
+                    IntPtr subMenuItem = User32.CreatePopupMenu();
+                    foreach (ContextMenuItem item in m_Items)
+                    {
+                        item.InsertMenuItem(subMenuItem);
+                    }
+                    User32.InsertMenu(hmenu, Position, (uint)(User32.MenuFlags.MF_BYPOSITION | User32.MenuFlags.MF_POPUP), subMenuItem, Text);
+                }
+                else
+                {
+                    User32.InsertMenu(hmenu, Position, (uint)User32.MenuFlags.MF_BYPOSITION, new IntPtr(User32.WM_USER + Id), Text);
+                }                
+            }
+        }
+    }
+}
---
+++ Forms/VariableTextBox.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Ketarin.Forms
+{
+    /// <summary>
+    /// A usual textbox plus variable selection tool.
+    /// </summary>
+    class VariableTextBox : TextBox
+    {
+        private string[] m_VariableNames = new string[0];
+        private ContextMenuCustomiser m_Customiser = null;
+
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets the variable names to show
+        /// within the context menu.
+        /// </summary>
+        public string[] VariableNames
+        {
+            get { return m_VariableNames; }
+            set {
+                if (m_VariableNames != value)
+                {
+                    m_VariableNames = value;
+                    RebuildContextMenu();
+                }
+            }
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Shortcut function to add multiple collection of variable
+        /// names at once.
+        /// </summary>
+        public void SetVariableNames(params string[][] variableNames)
+        {
+            List<string> varNames = new List<string>();
+
+            foreach (string[] names in variableNames)
+            {
+                varNames.AddRange(names);
+            }
+
+            VariableNames = varNames.ToArray();
+        }
+
+        protected override void OnHandleCreated(EventArgs e)
+        {
+            base.OnHandleCreated(e);
+
+            m_Customiser = new ContextMenuCustomiser(this);
+        }
+
+        /// <summary>
+        /// Adds all context menu items based on the given variable names.
+        /// </summary>
+        private void RebuildContextMenu()
+        {
+            m_Customiser.MenuItems.Clear();
+
+            if (m_VariableNames.Length == 0) return;
+
+            List<string> vars = new List<string>(m_VariableNames);
+            vars.Sort();
+            // We insert in reverse order
+            vars.Reverse();
+
+            // Add final separator
+            m_Customiser.MenuItems.Add(new ContextMenuItem(string.Empty, 0));
+
+            foreach (string var in vars)
+            {
+                ContextMenuItem newItem = new ContextMenuItem("{" + var + "}", 0);
+                newItem.EventHandler = OnVariableSelected;
+                m_Customiser.MenuItems.Add(newItem);
+            }
+        }
+
+        /// <summary>
+        /// Inserts the selected variable at the current cursor position.
+        /// </summary>
+        private void OnVariableSelected(ContextMenuItem menuItem)
+        {
+            int selStart = SelectionStart;
+            Text = Text.Insert(selStart, menuItem.Text);
+            SelectionStart = selStart + menuItem.Text.Length;
+        }
+    }
+}
