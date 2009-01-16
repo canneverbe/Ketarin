9fc8b21c24d3bf5e49abf7462bce9dcb245bddf4
floele <floele@users.noreply.github.com>
2009-01-16 13:29:14 +0000

    +Editor for multi line text boxes

---
+++ Forms/MultilineEditorDialog.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Ketarin.Forms
+{
+    partial class MultilineEditorDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bCancel = new System.Windows.Forms.Button();
+            this.bOK = new System.Windows.Forms.Button();
+            this.chkWordWrap = new System.Windows.Forms.CheckBox();
+            this.txtValue = new Ketarin.Forms.VariableTextBox();
+            this.SuspendLayout();
+            // 
+            // bCancel
+            // 
+            this.bCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.bCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.bCancel.Location = new System.Drawing.Point(286, 215);
+            this.bCancel.Name = "bCancel";
+            this.bCancel.Size = new System.Drawing.Size(75, 23);
+            this.bCancel.TabIndex = 3;
+            this.bCancel.Text = "Cancel";
+            this.bCancel.UseVisualStyleBackColor = true;
+            // 
+            // bOK
+            // 
+            this.bOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.bOK.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.bOK.Location = new System.Drawing.Point(205, 215);
+            this.bOK.Name = "bOK";
+            this.bOK.Size = new System.Drawing.Size(75, 23);
+            this.bOK.TabIndex = 2;
+            this.bOK.Text = "OK";
+            this.bOK.UseVisualStyleBackColor = true;
+            // 
+            // chkWordWrap
+            // 
+            this.chkWordWrap.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.chkWordWrap.AutoSize = true;
+            this.chkWordWrap.Location = new System.Drawing.Point(12, 221);
+            this.chkWordWrap.Name = "chkWordWrap";
+            this.chkWordWrap.Size = new System.Drawing.Size(78, 17);
+            this.chkWordWrap.TabIndex = 1;
+            this.chkWordWrap.Text = "&Word wrap";
+            this.chkWordWrap.UseVisualStyleBackColor = true;
+            this.chkWordWrap.CheckedChanged += new System.EventHandler(this.chkWordWrap_CheckedChanged);
+            // 
+            // txtValue
+            // 
+            this.txtValue.AcceptsReturn = true;
+            this.txtValue.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtValue.EnableEditor = false;
+            this.txtValue.Location = new System.Drawing.Point(12, 12);
+            this.txtValue.Multiline = true;
+            this.txtValue.Name = "txtValue";
+            this.txtValue.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtValue.Size = new System.Drawing.Size(349, 197);
+            this.txtValue.TabIndex = 0;
+            this.txtValue.WordWrap = false;
+            // 
+            // MultilineEditorDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(373, 250);
+            this.Controls.Add(this.txtValue);
+            this.Controls.Add(this.chkWordWrap);
+            this.Controls.Add(this.bOK);
+            this.Controls.Add(this.bCancel);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(350, 200);
+            this.Name = "MultilineEditorDialog";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Edit value";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bCancel;
+        private System.Windows.Forms.Button bOK;
+        private System.Windows.Forms.CheckBox chkWordWrap;
+        private VariableTextBox txtValue;
+    }
+}
---
+++ Forms/MultilineEditorDialog.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using CDBurnerXP.Forms;
+
+namespace Ketarin.Forms
+{
+    /// <summary>
+    /// This dialog serves as editor
+    /// for more flexible editing of multi line text fields.
+    /// </summary>
+    public partial class MultilineEditorDialog : PersistentForm
+    {
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets the value which is to be edited.
+        /// </summary>
+        public string Value
+        {
+            get
+            {
+                return txtValue.Text;
+            }
+            set
+            {
+                txtValue.Text = value;
+            }
+        }
+
+        #endregion
+
+        public MultilineEditorDialog()
+        {
+            InitializeComponent();
+
+            AcceptButton = bOK;
+            CancelButton = bCancel;
+        }
+
+        private void chkWordWrap_CheckedChanged(object sender, EventArgs e)
+        {
+            txtValue.WordWrap = chkWordWrap.Checked;
+        }
+
+        /// <summary>
+        /// Adds multiple collection of variable
+        /// names to the text box shown in the dialog.
+        /// </summary>
+        public void SetVariableNames(params string[][] variableNames)
+        {
+            this.txtValue.SetVariableNames(variableNames);
+        }
+    }
+}
