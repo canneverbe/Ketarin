674c17df8c9a37ff3f374493c72a85e70595f3ba
floele <floele@758197f2-88c6-11dd-83f7-11f20a0db3b5>
2009-02-01 14:13:34 +0000

    !Fixed UnauthorizedAccessExceptions

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.9.9.22")]
-[assembly: AssemblyFileVersion("0.9.9.22")]
+[assembly: AssemblyVersion("1.0.0.184")]
+[assembly: AssemblyFileVersion("1.0.0.184")]
