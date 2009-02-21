448af10551dcbec0d97bc38a08a9c50f275d2f98
floele <floele@users.noreply.github.com>
2009-02-21 14:56:22 +0000

    +Copy to clipboard in error dialog

--- Forms/ErrorsDialog.cs
+++ Forms/ErrorsDialog.cs
@@ -6,15 +6,23 @@ using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
 using CDBurnerXP.Forms;
+using CDBurnerXP.IO;
 
 namespace Ketarin.Forms
 {
+    /// <summary>
+    /// Represents a dialog which displays all errors 
+    /// which occured during an update process.
+    /// </summary>
     public partial class ErrorsDialog : PersistentForm
     {
         private ApplicationJobError[] m_Errors;
 
         #region Properties
 
+        /// <summary>
+        /// Gets or sets the errors which are to be shown in the dialog.
+        /// </summary>
         internal ApplicationJobError[] Errors
         {
             get { return m_Errors; }
@@ -36,6 +44,21 @@ namespace Ketarin.Forms
             base.OnLoad(e);
 
             olvErrors.SetObjects(m_Errors);
+        }
+
+        private void bCopyToClipboard_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            // Copy all errors to clipboard (separated with tabs and newlines)
+            foreach (ApplicationJobError error in m_Errors)
+            {
+                sb.Append(error.ApplicationJob.Name);
+                sb.Append("\t");
+                sb.AppendLine(error.Message);
+            }
+
+            SafeClipboard.SetData(sb.ToString(), true);
         }
     }
 }
