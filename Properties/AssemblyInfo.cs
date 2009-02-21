1bf8dfaf95dee9a4a0d07e993611f17ed2628567
floele <floele@users.noreply.github.com>
2009-02-21 13:11:44 +0000

    +Attribute "regex" and "value" for placeholders

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.197")]
-[assembly: AssemblyFileVersion("1.0.1.197")]
+[assembly: AssemblyVersion("1.0.2.215")]
+[assembly: AssemblyFileVersion("1.0.2.215")]
