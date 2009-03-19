6be923f4220859921272f7b4ea3f378b2e091824
floele <floele@users.noreply.github.com>
2009-03-19 18:59:25 +0000

    !Fixed unnecessary errors

--- UrlVariable.cs
+++ UrlVariable.cs
@@ -3,7 +3,6 @@ using System.IO;
 using System.Collections.Generic;
 using System.Text;
 using System.Data;
-using System.Net;
 using System.Xml;
 using System.Xml.Serialization;
 using System.Text.RegularExpressions;
