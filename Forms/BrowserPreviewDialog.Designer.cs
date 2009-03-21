71a7d60defa8051dd1fae2a0bfa498a648c8b890
floele <floele@users.noreply.github.com>
2009-03-21 11:47:52 +0000

    +Webbrowser preview

---
+++ Forms/BrowserPreviewDialog.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace Ketarin.Forms
+{
+    partial class BrowserPreviewDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.ScriptErrorsSuppressed = true;
+            this.webBrowser.Size = new System.Drawing.Size(413, 338);
+            this.webBrowser.TabIndex = 0;
+            this.webBrowser.WebBrowserShortcutsEnabled = false;
+            // 
+            // BrowserPreviewDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(413, 338);
+            this.Controls.Add(this.webBrowser);
+            this.MinimizeBox = false;
+            this.Name = "BrowserPreviewDialog";
+            this.SavePosition = true;
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Webbrowser Preview";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser;
+    }
+}
---
+++ Forms/BrowserPreviewDialog.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Ketarin.Forms
+{
+    /// <summary>
+    /// Represents a dialog, which visualises the HTML code
+    /// loaded by a variable.
+    /// </summary>
+    public partial class BrowserPreviewDialog : CDBurnerXP.Forms.PersistentForm, IComponent
+    {
+        public BrowserPreviewDialog()
+        {
+            InitializeComponent();
+        }
+
+        public void ShowPreview(IWin32Window owner, string url, string postData)
+        {
+            if (!Visible)
+            {
+                Show(owner);
+            }
+
+            if (String.IsNullOrEmpty(postData))
+            {
+                webBrowser.Navigate(url, "", null, null);
+            }
+            else
+            {
+                webBrowser.Navigate(url, "", Encoding.ASCII.GetBytes(postData), "Content-Type: application/x-www-form-urlencoded");
+            }
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            base.OnClosing(e);
+
+            // Prevent disposing
+            e.Cancel = true;
+            Hide();
+        }
+    }
+}
