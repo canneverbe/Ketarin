f046cd78780220ad7decb693eec7ec72ab680b7e
floele <floele@758197f2-88c6-11dd-83f7-11f20a0db3b5>
2010-09-19 21:38:45 +0000

    Compiler

---
+++ ScriptType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ketarin
+{
+    /// <summary>
+    /// Scripting language used for a command.
+    /// </summary>
+    public enum ScriptType
+    {
+        /// <summary>
+        /// C# script
+        /// </summary>
+        CS,
+        /// <summary>
+        /// Batch file
+        /// </summary>
+        Batch
+    }
+}
