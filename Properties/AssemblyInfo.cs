967393a9f1b7deebe16279d0c54042eecaedc69c
floele <floele@758197f2-88c6-11dd-83f7-11f20a0db3b5>
2010-10-10 20:31:54 +0000

    Version update

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.5.0.345")]
-[assembly: AssemblyFileVersion("1.5.0.345")]
+[assembly: AssemblyVersion("1.5.0.400")]
+[assembly: AssemblyFileVersion("1.5.0.400")]
