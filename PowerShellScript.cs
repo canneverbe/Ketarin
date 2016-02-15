03e06f15d3b64bf15b9b43b77f6f950c8764ee5c
floele <floele@758197f2-88c6-11dd-83f7-11f20a0db3b5>
2016-02-15 17:53:15 +0000

    +"globalvars" collection in PS

---
+++ PowerShellScript.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Management.Automation;
+using System.Text;
+using Ketarin.Forms;
+
+namespace Ketarin
+{
+    internal class PowerShellScript
+    {
+        private readonly string scriptText;
+
+        public PowerShellScript(string scriptText)
+        {
+            this.scriptText = scriptText;
+        }
+
+        internal void Execute(ApplicationJob application)
+        {
+            using (PowerShell powerShell = PowerShell.Create())
+            {
+                powerShell.AddScript(this.scriptText);
+
+                // Make application object available to the script.
+                powerShell.Runspace.SessionStateProxy.SetVariable("app", application);
+                powerShell.Runspace.SessionStateProxy.SetVariable("globalvars", UrlVariable.GlobalVariables);
+
+                Collection<PSObject> psOutput = powerShell.Invoke();
+
+                if (powerShell.HadErrors)
+                {
+                    StringBuilder sb = new StringBuilder();
+                    foreach (ErrorRecord error in powerShell.Streams.Error)
+                    {
+                        sb.AppendLine(error.Exception.Message);
+                    }
+
+                    throw new ApplicationException(sb.ToString());
+                }
+
+                foreach (PSObject outputItem in psOutput)
+                {
+                    // if null object was dumped to the pipeline during the script then a null
+                    // object may be present here. check for null to prevent potential NRE.
+                    if (outputItem != null)
+                    {
+                        LogDialog.Log("PowerShell: " + outputItem);
+                    }
+                }
+
+                // Output all information we can get.
+                foreach (ErrorRecord warning in powerShell.Streams.Error)
+                {
+                    LogDialog.Log("PowerShell (error): " + warning.ErrorDetails.Message);
+                }
+
+                foreach (WarningRecord warning in powerShell.Streams.Warning)
+                {
+                    LogDialog.Log("PowerShell (warning): " + warning.Message);
+                }
+
+                foreach (InformationRecord info in powerShell.Streams.Information)
+                {
+                    LogDialog.Log("PowerShell (info): " + info.MessageData);
+                }
+            }
+        }
+    }
+}
