3ad455a1b497e67930bfa4f3abd64b9c75d1cf61
floele <floele@758197f2-88c6-11dd-83f7-11f20a0db3b5>
2016-08-16 18:48:20 +0000

    Added segmented downloading for FTP protocol

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.8.6.600")]
-[assembly: AssemblyFileVersion("1.8.6.600")]
+[assembly: AssemblyVersion("1.8.6.700")]
+[assembly: AssemblyFileVersion("1.8.6.700")]
