8e2a73766d3d985825fb3a4eb00d8f966bf143b4
floele <floele@users.noreply.github.com>
2018-11-03 17:06:09 +0100

    Added nuget references

--- Updater.cs
+++ Updater.cs
@@ -484,7 +484,7 @@ namespace Ketarin
                     catch (SQLiteException ex)
                     {
                         // If "locked" exception (slow USB device eg.) continue trying
-                        if (ex.ErrorCode == SQLiteErrorCode.Locked)
+                        if (ex.ErrorCode == (int)SQLiteErrorCode.Locked)
                         {
                             numTries--;
                             LogDialog.Log(job, ex);
