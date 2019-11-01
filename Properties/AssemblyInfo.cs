9ae422bc808d357141bdf9cb29d9625f88c6385f
floele <floele@users.noreply.github.com>
2019-11-01 13:52:35 +0100

    Update AssemblyInfo.cs
    Updated AssemblyCopyright

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Canneverbe Limited")]
 [assembly: AssemblyProduct("Ketarin")]
-[assembly: AssemblyCopyright("© Canneverbe Limited 2008-2018")]
+[assembly: AssemblyCopyright("© Canneverbe Limited 2008-2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
